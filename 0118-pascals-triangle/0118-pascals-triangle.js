980413ffb3a6da767842dbd4f3725d3bdd99091a
Dan White <66431847+daniel-maxwell@users.noreply.github.com>
2023-05-25 19:08:10 +0100

    Time: 54 ms (85.04%), Space: 42.5 MB (41.38%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    let output = [];
+    for (let i=0; i<numRows; i++){
+        const rowSize = i+1;
+        let row = [];
+        for (let j=0; j<rowSize; j++) {
+            j === 0 || j === rowSize-1 ? row.push(1): row.push(output[i-1][j-1] + output[i-1][j]);
+        }
+        output.push(row)
+    }
+    return output
+};
