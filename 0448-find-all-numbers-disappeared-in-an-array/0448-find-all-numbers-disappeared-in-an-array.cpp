3067acc3481c503a2a1289b40f5e2ac26247e625
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2023-06-23 15:25:19 +0100

    Time: 69 ms (50.08%), Space: 33.7 MB (58.38%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        set<int> range = {};
-        set<int> numsSet = {};
-        int count = 1;
-        for (int n : nums) {
-            range.insert(count);
-            ++count;
-            numsSet.insert(n);
-        };
-        nums = {};
-        set_difference(range.begin(), range.end(), numsSet.begin(), numsSet.end(), std::inserter(nums, nums.end()));
-        return nums;
-    };
+        vector<int> ans;
+        
+        for(int x : nums){
+            int currentVal = abs(x);
+            nums[currentVal-1] = 0 - abs(nums[currentVal-1]);
+        }
+        
+        for(int i = 1; i <= nums.size(); i++)
+            if(nums[i-1] > 0) ans.push_back(i);
+        
+        return ans;
+    }
 };
