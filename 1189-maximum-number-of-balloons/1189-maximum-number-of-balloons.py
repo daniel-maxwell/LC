ab19045121e59a6f068336bae27e17ed63fba89d
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2023-06-24 12:39:38 +0100

    Time: 53 ms (36.88%), Space: 16.3 MB (93.72%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -5,15 +5,11 @@ class Solution:
             'a': 0,
             'l': 0,
             'o': 0,
-            'n': 0,
+            'n': 0
         }
-
         for i in range(0, len(text)):
             if text[i] in charCounts:
                 charCounts[text[i]] += 1
-
         charCounts['l'] = math.floor(charCounts['l']/2)
         charCounts['o'] = math.floor(charCounts['o']/2)
-
         return min(charCounts.values())
-        
