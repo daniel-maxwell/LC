bc60f396872920c5e7091e6029a15666b523f52b
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2023-07-05 11:05:51 +0100

    Time: 4 ms (86.35%), Space: 10.7 MB (99.88%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int l = 0, r = 0;
+        while (r < nums.size()) {
+            int count = 1;
+            while (r + 1 < nums.size() && nums[r] == nums[r + 1]) {
+                ++r;
+                ++count;
+            };
+            for (int i = 0; i < min(2, count); ++i) {
+                nums[l] = nums[r];
+                ++l;
+            };
+            ++r;
+        };
+        return l;
+    };
+};
