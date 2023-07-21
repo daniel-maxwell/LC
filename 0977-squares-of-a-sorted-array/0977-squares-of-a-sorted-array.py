b56d2964fac9832ff4eb326d298f04538a30463f
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2023-07-21 10:14:43 +0100

    Time: 292 ms (9.16%), Space: 18.7 MB (21.66%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,32 +1,16 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        negSq = [i**2 for i in nums if i < 0]
-        nums = nums[len(negSq):]
-
-        if len(nums) > 0:
-            i = 0
-            nums[i] **= 2
-            
-            while len(negSq) > 0 and nums[i] > negSq[-1]:
-                nums = nums[:i] + [negSq.pop()] + nums[i:]
-                i += 1
-                
-            while i < len(nums) - 1:
-                j = i + 1
-                nums[j] **= 2
-
-                while len(negSq) > 0 and negSq[-1] <= nums[j]:
-                    nums = nums[:j] + [negSq.pop()] + nums[j:]
-                    i += 1
-                    j += 1
-                i += 1
-
-            if len(negSq) > 0:
-                negSq.reverse()
-                nums = nums + negSq
-
-        else:
-            negSq.reverse()
-            return negSq
+        res = [-1] * len(nums)
+        l, r, x = 0, len(nums) -1, len(nums) -1
         
-        return nums
+        while l <= r:
+            if nums[l] ** 2 > nums[r] ** 2:
+                res[x] = nums[l] ** 2
+                l += 1
+            else:
+                res[x] = nums[r] ** 2
+                r -= 1
+            x -= 1
+ 
+        return res
+            
