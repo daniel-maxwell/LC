8acf8d61b62787098304cf55f7574893c053ed81
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2023-08-15 17:36:55 +0100

    Time: 1280 ms (21.06%), Space: 50.8 MB (81.53%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -7,8 +7,8 @@ var containsNearbyDuplicate = function(nums, k) {
     for (let i=0; i<nums.length; i++) {
         for (let j=i+1; j<nums.length; j++) {
             if ((Math.abs(i-j)) > k) break;
-            if (nums[i] === nums[j]) return true
-        }
-    }
-    return false
-}
+            if (nums[i] === nums[j]) return true;
+        };
+    };
+    return false;
+};
