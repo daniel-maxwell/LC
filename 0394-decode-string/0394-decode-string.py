8e0bc063e43d38dc04c076b0a42c335901041438
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2023-09-04 22:07:45 +0100

    Time: 30 ms (95.32%), Space: 16.5 MB (37.14%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,19 +1,17 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-
-        nums = set(['0','1','2','3','4','5','6','7','8','9'])
         stack, coef, output, curr = [], '', '', ''
 
         for i in range(0, len(s)):
 
             if s[i] == ']':
-                while stack[-1] not in nums:
+                while not stack[-1].isdigit():
                     if stack[-1] is not '[':
                         curr = stack.pop() + curr
                     else:
                         stack.pop()
 
-                while stack and stack[-1] in nums:
+                while stack and stack[-1].isdigit():
                     coef = stack.pop() + coef
 
                 curr = int(coef) * curr
