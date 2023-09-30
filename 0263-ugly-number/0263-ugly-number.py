f0aedeb95772a18dbd7be57beafdc21490c8d2cd
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2023-09-30 20:23:20 +0100

    Time: 47 ms (12.16%), Space: 16.3 MB (53.00%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -1,20 +1,9 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
-        if n < 1: return False
-        uglySet = set([2, 3, 5])
-
-        i = 2
-        factors = []
-        while i * i <= n:
-            if n % i:
-                i += 1
-            else:
-                n //= i
-                print(i)
-                if i not in uglySet:
-                    return False
-        if n > 1:
-            if n not in uglySet:
-                return False
-
-        return True
+        if n <= 0:
+            return False
+        
+        for p in [2, 3, 5]:
+            while n % p == 0:
+                n = n // p
+        return n == 1
