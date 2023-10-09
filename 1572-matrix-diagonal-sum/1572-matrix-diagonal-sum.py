95fc9dad2c8a69273b1d69f2e8e890cedbfe9222
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2023-10-09 21:22:20 +0100

    Time: 106 ms (51.41%), Space: 16.5 MB (62.39%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -1,9 +1,7 @@
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:        
         subtractMid = 0 if len(mat) % 2 == 0 else mat[len(mat)//2][len(mat)//2]
-
-        offset = 0
-        sum = 0
+        offset, sum = 0, 0
 
         for i in range(0, len(mat)):
             sum += mat[i][offset]
