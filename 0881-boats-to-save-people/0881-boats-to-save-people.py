0edd115565c05fe5ae7d8f6d90f7dd142ef14ba1
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2023-10-16 02:25:33 +0100

    Time: 383 ms (80.95%), Space: 23.1 MB (93.52%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -1,41 +1,11 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
         people.sort()
-        numBoats, rescued = 0, 0
-        numToRescue = len(people)
-
-        while rescued < numToRescue:
-            heaviest = people.pop()
-            rescued += 1
-            peopleToCheck = len(people)
-
-            # Select heaviest person under weight limit (if any)
-            # Binary search
-
-            l, r = 0, len(people) - 1
-            mid = l + (r - l)//2
-
-            while peopleToCheck >= 1 and mid >= 0:
-                
-                combinedWeight = people[mid] + heaviest
-
-                if combinedWeight <= limit:
-                    if mid+1 >= len(people):
-                        break
-                    elif people[mid+1] + heaviest > limit:
-                        break
-                    else:
-                        l = mid + 1
-                else:
-                    r = mid - 1
-                    
-                mid = l + (r - l)//2               
-                peopleToCheck -= 1
-            
-            if peopleToCheck > 0 and mid >= 0: # We found a second person
-                people.pop(mid)
-                rescued += 1
-            
-            numBoats += 1
-
-        return numBoats
+        right = len(people) - 1
+        left = res = 0
+        while left <= right:
+            if people[left] + people[right] <= limit:
+                left += 1
+            right -= 1
+            res += 1
+        return res
