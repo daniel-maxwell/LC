d64e01544009c553ae742b7d628175eef3885e1f
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2023-10-25 16:07:41 +0100

    Time: 2793 ms (50.21%), Space: 36.8 MB (16.25%) - LeetHub

--- 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.py
+++ 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.py
@@ -1,44 +1,25 @@
 class Solution:
     def maximumRemovals(self, s: str, p: str, removable: List[int]) -> int:
-        def isSubsequence(s, p):
-            l = 0
-            for char in s:
-                if l == len(p):
-                    return True
-                if char == p[l]:
-                    l += 1
-            return l >= len(p)
-
-        l, r, res = 0, len(removable) - 1, 0
-        m = l + ((r-l) // 2)
-        currS, prevM = s, m
-        i = 0
-
-        while i <= m:
-            currS = currS[:removable[i]] + "0" + currS[removable[i]+1:]
-            i += 1
-
-        while l <= r:
-
-            if isSubsequence(currS, p):
-                if m + 1 > res: res = m + 1
-                l = m + 1
-            else:
-                r = m - 1
-
-            prevM = m
-            m = l + ((r-l) // 2)
-
-            if m < prevM:
-                start = prevM
-                stop = m
-                for i in range(m+1, prevM+1):
-                    currS = currS[:removable[i]] + s[removable[i]] + currS[removable[i]+1:]
-
-            else:
-                start = prevM + 1
-                stop = m
-                for i in range(start, stop+1):
-                    currS = currS[:removable[i]] + "0" + currS[removable[i]+1:]
-
-        return res
+        def isSubsequence(removeIndices): 
+            i, j = 0, 0
+            while i < len(p) and j < len(s): 
+                if j in removeIndices: 
+                    j += 1
+                elif p[i] == s[j]: 
+                    i += 1
+                    j += 1
+                else: 
+                    j += 1
+            return i == len(p)
+        
+        l, r = 0, len(removable)
+        maxK = 0
+        while l <= r: 
+            k = (l + r) // 2
+            removeIndices = set(removable[:k])
+            if isSubsequence(removeIndices): 
+                maxK = max(maxK, k)
+                l = k + 1
+            else: 
+                r = k - 1
+        return maxK
