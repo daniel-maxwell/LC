b7f3b384a8f58c35f588e005f1f06d1fda65f047
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2023-11-03 14:11:04 +0000

    Time: 493 ms (73.69%), Space: 22.5 MB (17.22%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,12 +1,5 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-
-        res = None
-
-        # Binary search: have left and right pointers initialized to lowest and highest values
-        # In this case, lowest weight capacity must be 1 (can carry a weight of 1 per day)
-        # Highest must be max of weights * len(weights) /// can this be optimised?
- 
         def checkCapacity(weights, days, capacity):
             dailyCapacity = capacity
             
@@ -18,14 +11,12 @@ class Solution:
                     if not days: return False
                     dailyCapacity = capacity
                     dailyCapacity -= weight
-
                 else:
                     dailyCapacity -= weight
 
             return True
 
-        l, r = 1, 500 * len(weights)
-
+        l, r, result = 1, 500 * len(weights), None
         mid = l + ((r-l)//2)
 
         while l <= r:
@@ -33,10 +24,8 @@ class Solution:
             if checkCapacity(weights, days, mid):
                 res = mid
                 r = mid - 1
-
             else:
                 l = mid + 1
-
             mid = l + ((r-l)//2)
 
         return res
