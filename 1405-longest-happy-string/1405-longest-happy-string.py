c12d44a6d744cc3f4bf2c8910472c25c4898182f
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2023-12-05 14:04:49 +0000

    Time: 39 ms (55.30%), Space: 16.5 MB (7.83%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.py
+++ 1405-longest-happy-string/1405-longest-happy-string.py
@@ -3,22 +3,22 @@ class Solution:
         counts, letters = [-a, -b, -c], ["a", "b", "c"]
         maxHeap = [list(t) for t in zip(counts, letters) if t[0] < 0]
         heapq.heapify(maxHeap)
-        prev, res = None, ""
+        prev, res = None, []
 
         while len(maxHeap) > 1:
             top = heapq.heappop(maxHeap)
             second = heapq.heappop(maxHeap)
 
             if abs(top[0]) - abs(second[0]) >= 2:
-                res += 2 * top[1]
+                res.append(2 * top[1])
                 top[0] += 2
-                res += second[1]
+                res.append(second[1])
                 second[0] += 1
 
             else:
-                res += top[1]
+                res.append(top[1])
                 top[0] += 1
-                res += second[1]
+                res.append(second[1])
                 second[0] += 1
 
             if top[0] <= -1: heapq.heappush(maxHeap, top)
@@ -27,8 +27,8 @@ class Solution:
         if maxHeap:
             last = heapq.heappop(maxHeap)
             if last[0] < -1:
-                return res + (2 * last[1])
+                return ''.join(res) + (2 * last[1])
             else:
-                return res + last[1]
+                return ''.join(res) + last[1]
         else:
-            return res
+            return ''.join(res)
