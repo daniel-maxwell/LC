72533d27ab6bfe3ceb89f5f2254fc74e839fff7e
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-01-01 16:42:11 +0000

    Time: 8958 ms (5.01%), Space: 17.3 MB (29.98%) - LeetHub

---
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.py
@@ -0,0 +1,20 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        maxSL = sum(matchsticks) / 4
+        if not maxSL.is_integer(): return False
+        square = [0, 0, 0, 0]
+        matchsticks.sort(reverse=True)
+
+        def backtrack(i):
+            if i == len(matchsticks):
+                return True
+
+            for j in range(4):
+                if square[j] + matchsticks[i] <= maxSL:
+                    square[j] += matchsticks[i]
+                    if backtrack(i+1):
+                        return True
+                    square[j] -= matchsticks[i]
+            return False
+
+        return backtrack(0)
