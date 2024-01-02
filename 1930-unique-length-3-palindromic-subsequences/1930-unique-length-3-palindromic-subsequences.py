4a9738ec17b5f2fdf9e5f044304a6e06de205c72
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-01-02 19:58:21 +0000

    Time: 188 ms (71.22%), Space: 18.2 MB (17.17%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -1,27 +1,8 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        def indexNoExcept(self, char, lst, start=0):
-            for i in range(start, len(lst)):
-                if lst[i] == char:
-                    return i
-            return None
-            
-        found, s, result = set(), deque(list(s)), 0
-
-        while len(s) > 2:
-            first = s.popleft()
-            lastIdx = indexNoExcept(self, first, s)
-            if lastIdx == None: continue
-
-            for i in range(0, lastIdx):
-                pal = first + s[i] + s[lastIdx]
-                if pal not in found:
-                    found.add(pal)
-                    result += 1
-
-            if first + first + first not in found:
-                if indexNoExcept(self, first, s, lastIdx+1):
-                    found.add(first + first + first)
-                    result += 1
-
-        return result
+        count = 0
+        chars = set(s)
+        for char in chars:
+            first,last = s.find(char),s.rfind(char)
+            count += len(set(s[first+1:last]))
+        return count
