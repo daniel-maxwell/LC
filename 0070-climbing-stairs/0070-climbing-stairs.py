dad71897c4022fd9f0393e32dbf2ce6a8d21a5ce
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-02-17 11:57:17 +0700

    Time: 38 ms (38.75%), Space: 16.5 MB (77.57%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
-        steps = [1, 2, 3]
+        totalWays = [1, 2, 3]
 
-        if n <= len(steps):
-            return steps[n-1]
+        if n <= 3:
+            return totalWays[n-1]
 
-        for i in range(2, n-1):
-            steps.append(steps[-1] + steps[-2])
+        else:
+            while len(totalWays) < n:
+                totalWays.append(totalWays[-1] + totalWays[-2])
 
-        return steps[-1]      
+            return totalWays[-1]
