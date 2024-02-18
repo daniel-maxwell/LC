72c95cc46d4fc577b8151ae96b632591020e9533
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-02-18 21:38:12 +0700

    Time: 39 ms (95.76%), Space: 17 MB (52.32%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,14 +1,24 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        for i in range(0, len(nums1)):  
-            for j in range(0, len(nums2)):
-                if nums1[i] == nums2[j]:
-                    for k in range(j+1, len(nums2)):
-                        if nums2[j] < nums2[k]:
-                            nums1[i] = nums2[k]
-                            break
-                    break             
-            if nums1[i] == nums2[j]: nums1[i] = -1 
-        return nums1
-                           
-        
+
+        res, mapping = [], {}
+        maxEl = nums2[-1]
+
+        for i in range(len(nums2)-1, -1, -1):
+            if nums2[i] >= maxEl:
+                maxEl = nums2[i]
+                mapping[nums2[i]] = -1
+
+            else:
+                j = i
+                while j < len(nums2) and nums2[j] <= nums2[i]:
+                    j += 1
+                if j == len(nums2):
+                    mapping[nums2[i]] = -1
+                else:
+                    mapping[nums2[i]] = nums2[j] 
+
+        for el in nums1:
+            res.append(mapping[el])
+
+        return res
