848f52bd1c21f51d49bdb38f4b7ed16ff4d7c228
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-02-18 21:22:54 +0700

    Time: 134 ms (54.84%), Space: 16.9 MB (67.73%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,30 +1,23 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        if n == 0: return True
-        if (len(flowerbed) == 1):
-            return flowerbed[0] == 0 and n <= 1
-        if n >= len(flowerbed): return False
-        if (len(flowerbed) == 2):
-            return sum(flowerbed[:]) < n
-        if (len(flowerbed) == 3):
-            return sum(flowerbed[:2]) < n or sum(flowerbed[1:]) < n
-
-        availableSpaces = 0
-
-        for i in range(1, len(flowerbed)-1):
-
-            if i == 1:
-                if flowerbed[i-1] + flowerbed[i] == 0:
-                    flowerbed[i-1] = 1
-                    availableSpaces += 1
-
-            elif i == len(flowerbed)-2:
+        
+        if len(flowerbed) < 2:
+            return n == 0 or sum(flowerbed) == 0 
+        
+        for i in range(len(flowerbed)):
+            if i == 0:
                 if flowerbed[i] + flowerbed[i+1] == 0:
-                    flowerbed[i+1] = 1
-                    availableSpaces += 1
+                    flowerbed[i] = 1
+                    n -= 1
+
+            elif i == len(flowerbed) -1:
+                if flowerbed[i-1] + flowerbed[i] == 0:
+                    n -= 1
+                    flowerbed[i] = 1
+
 
             elif flowerbed[i-1] + flowerbed[i] + flowerbed[i+1] == 0:
+                n -= 1
                 flowerbed[i] = 1
-                availableSpaces += 1
 
-        return availableSpaces >= n
+        return n <= 0
