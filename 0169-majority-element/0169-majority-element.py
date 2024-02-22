c1ae019f4dee7a6412661e9d44734f2f0ef85af2
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-02-23 04:49:30 +0700

    Time: 170 ms (12.26%), Space: 18.1 MB (27.18%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,13 +1,18 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        occurrences = {}
-        maxOccurrence = [nums[0],1]
-        
-        for i in range (0, len(nums)):   
-            if nums[i] in occurrences:
-                occurrences[nums[i]] += 1
-                if occurrences[nums[i]] > maxOccurrence[1]:
-                    maxOccurrence = [nums[i], occurrences[nums[i]]]
-            else: occurrences[nums[i]] = 1
-        
-        return maxOccurrence[0]
+        occurences = {nums[0]:1}
+        maxEl = nums[0]
+
+        if len(nums) < 2:
+            return maxEl
+
+        for num in nums[1:]:
+            if num in occurences:
+                occurences[num] += 1
+            else:
+                occurences[num] = 1
+
+            if occurences[num] > occurences[maxEl]:
+                maxEl = num
+
+        return maxEl
