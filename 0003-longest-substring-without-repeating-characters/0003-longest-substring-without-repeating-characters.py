38ac3321f1f46d633e53b16da2826ae14ea36442
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-02-26 17:42:12 +0700

    Time: 55 ms (64.27%), Space: 16.7 MB (75.90%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,31 +1,27 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-
         if len(s) == 0: return 0
-        elif len(s) == 1: return 1
+        if len(s) == 1: return 1
 
         maxLength, currLength = 1, 1
-        charSet = set([s[0]])
-        i, j = 0, 1
-
-        while j < len(s):
-
-            if s[j] not in charSet:
+        l, r = 0, 1
+        charSet = set(s[l])
+        
+        while r < len(s):
+            if s[r] not in charSet:
+                charSet.add(s[r])
                 currLength += 1
-                charSet.add(s[j])
+                r += 1
+
+            elif r - l > 1:
+                charSet.remove(s[l])
+                currLength -= 1
+                l += 1
 
             else:
-                if s[i] == s[j]:
-                    i += 1
-                else:
-                    while s[i] != s[j]:
-                        charSet.remove(s[i])
-                        i += 1
-                    i += 1
-                    currLength = (j - i) + 1
-
-            if currLength > maxLength: maxLength = currLength
-
-            j += 1
-
-        return max(maxLength, len(charSet))
+                l = r
+                r += 1
+            
+            maxLength = max(currLength, maxLength)
+        
+        return maxLength
