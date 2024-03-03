130e9fa34639ad92eb16f64498f7b9636bf12d3a
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-03-03 21:59:34 +0700

    Time: 1540 ms (5.03%), Space: 20.8 MB (38.30%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,14 +1,37 @@
-from sortedcontainers import SortedList
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mapping = {}
-        for i in range(0, len(strs)):
-            hashWord = SortedList([])
-            for letter in strs[i]:
-                hashWord.add(ord(letter))
-            hashWord = repr(hashWord)
-            if hashWord in mapping:
-                mapping[hashWord].append(strs[i])
+        charCounts, res = {}, []
+        wordCounts = {}
+
+        for word in strs:
+            if word in wordCounts:
+                wordCounts[word] += 1
             else:
-                mapping[hashWord] = [strs[i]]
-        return mapping.values()
+                wordCounts[word] = 1
+
+            charCounts[word] = {}
+            for char in word:
+                if char in charCounts[word]:
+                    charCounts[word][char] += 1
+                else:
+                    charCounts[word][char] = 1
+
+        while charCounts:
+            currWord = strs.pop()
+            if currWord not in charCounts:
+                continue
+            currDict = charCounts.pop(currWord)
+            group = [currWord] * wordCounts[currWord]
+            groupSize = len(group)
+
+            for word, charCount in charCounts.items():
+                if currDict == charCount:
+                    group.append(word)
+
+            if len(group) > groupSize:
+                for i in range(groupSize, len(group)):
+                    charCounts.pop(group[i])
+
+            res.append(group)
+
+        return res
