e52cb5a6293a2afe01842f4e515723091b6362a6
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-03-06 17:26:15 +0700

    Time: 43 ms (8.29%), Space: 16.5 MB (64.39%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,18 +6,18 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        self.bool = True
 
-        def traverseTwo(root1, root2):
-            if root1 and root2:
-                if root1.val != root2.val:
-                    self.bool = False
-                    return
-                traverseTwo(root1.left, root2.left)
-                traverseTwo(root1.right, root2.right)
-            elif root1 or root2:
-                self.bool = False
-                return
+        def dfs(r1, r2):
+            if not r1 and not r2:
+                return True
 
-        traverseTwo(p, q)
-        return self.bool
+            if (not r1 and r2) or (r1 and not r2):
+                return False
+
+            if r1.val != r2.val:
+                return False
+
+            return True and dfs(r1.left, r2.left) and dfs(r1.right, r2.right)
+
+        return dfs(p, q)
+        
