69df68c3738647762204a207d6815fc3abeb36c3
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-03-10 18:52:34 +0700

    Time: 44 ms (8.63%), Space: 16.5 MB (57.20%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.py
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.py
@@ -1,5 +1,3 @@
-
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -8,34 +6,26 @@
 #         self.right = right
 class Solution:
     def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        nodes = []
 
-        self.set = []
-
-        def preorder(root):
-            if root == None:
+        def dfs(root):
+            if not root:
                 return
-            self.set.append(root.val)
-            preorder(root.left)
-            preorder(root.right)
 
+            nodes.append(root.val)
+            dfs(root.left)
+            dfs(root.right)
 
-        preorder(root.left)
-        self.set.append(root.val)
-        preorder(root.right)
+        dfs(root)
+        nodes.sort()
 
-        def findMinDiff(arr):
-            n = len(arr)
-            # Initialize difference as infinite
-            diff = 10**20
-        
-            # Find the min diff by comparing difference
-            # of all possible pairs in given array
-            for i in range(n-1):
-                for j in range(i+1, n):
-                    if abs(arr[i]-arr[j]) < diff:
-                        diff = abs(arr[i] - arr[j])
-        
-            # Return min diff
-            return diff
-        
-        return findMinDiff(self.set)
+        minDiff = nodes[1] - nodes[0]
+
+        i, j = 1, 2
+
+        while j < len(nodes):
+            minDiff = min(minDiff, nodes[j] - nodes[i])
+            i += 1
+            j += 1
+
+        return minDiff
