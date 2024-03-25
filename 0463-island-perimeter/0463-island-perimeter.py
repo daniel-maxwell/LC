650f6280cb6c34cad685cd49116b5be079ccd88e
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-03-25 09:30:31 +0700

    Time: 482 ms (16.63%), Space: 18.1 MB (26.65%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,16 +1,40 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+        def isValidMove(coord):
+            if (
+                coord[0] < 0 or
+                coord[0] >= ROWS or
+                coord[1] < 0 or
+                coord[1] >= COLS or
+                grid[coord[0]][coord[1]] == 0
+            ):
+                return False
+            else:
+                return True
+
+        def getPerimiter(coord):
+            visited = set([coord])
+            perimiter = 0
+            q = deque([coord])
+            
+            while q:
+                curr = q.pop()
+                for dc, dr in directions:
+                    move = (curr[0] + dc, curr[1] + dr)
+
+                    if isValidMove(move):
+                        if move not in visited:
+                            q.appendleft(move)
+                    else:
+                        perimiter += 1
+
+                    visited.add(move)
+            return perimiter
         
-        for i in range(0, len(grid)):
-            grid[i] = [0] + grid[i] + [0]
-
-        grid = [[0]* len(grid[0])] + grid + [[0]* len(grid[0])]
-
-        perimiter = 0
-
-        for i in range(1, len(grid)-1):
-            for j in range(1, len(grid[1])-1):
+        for i in range(ROWS):
+            for j in range(COLS):
                 if grid[i][j] == 1:
-                    perimiter += 4 - (grid[i-1][j] + grid[i+1][j] + grid[i][j-1] + grid[i][j+1])
-
-        return perimiter
+                    return getPerimiter((i, j))
