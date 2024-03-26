c7665c4a362e162840a7d7eee53b3ef32eedd184
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-03-26 14:34:05 +0700

    Time: 86 ms (68.44%), Space: 17.9 MB (12.77%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,38 +1,41 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        pDict, sDict = {}, {}
-        pLength = len(p)
+        if len(p) > len(s): return []
 
-        if pLength > len(s): return []
-        
-        for i in range(0, pLength):
-            if p[i] in pDict: pDict[p[i]] += 1
-            else: pDict[p[i]] = 1
+        pCharCounts, sCharCounts = {}, {}
 
-            if s[i] in sDict: sDict[s[i]] += 1
-            else: sDict[s[i]] = 1
-
-        res = [0] if pDict == sDict else []
-        
-        i = 1
-        j = pLength
-        while j < len(s):
-            if sDict[s[i-1]] > 1:
-                sDict[s[i-1]] -= 1
+        for char in p:
+            if char in pCharCounts:
+                pCharCounts[char] += 1
             else:
-                del sDict[s[i-1]]
-            
-            if s[j] in sDict: sDict[s[j]] += 1
-            else: sDict[s[j]] = 1
+                pCharCounts[char] = 1
 
-            if sDict == pDict:
-                res.append(i)
-                
-            i += 1
-            j += 1
+        for i in range(len(p)):
+            if s[i] in sCharCounts:
+                sCharCounts[s[i]] += 1
+            else:
+                sCharCounts[s[i]] = 1
+
+        l, r, res = 0, len(p), []
+
+        while r < len(s):
             
+            if sCharCounts == pCharCounts:
+                res.append(l)
+            
+            sCharCounts[s[l]] -= 1
+            if sCharCounts[s[l]] == 0:
+                sCharCounts.pop(s[l])
+
+            if s[r] in sCharCounts:
+                sCharCounts[s[r]] += 1
+            else:
+                sCharCounts[s[r]] = 1
+
+            l += 1
+            r += 1
+
+        if sCharCounts == pCharCounts:
+            res.append(l)
+
         return res
-    
-    # What if instead we had a dict initialised with all the letters mapped to occurences of p
-    # Each time we loop through the substring we subtract 1 when theres a matching char
-    # If all values are 0 when we're finished looping we found a match
