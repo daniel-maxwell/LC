cbcea862139a3f9a0f2bd301c99713639220667e
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-04-27 12:56:43 +0700

    Time: 33 ms (82.15%), Space: 16.5 MB (70.13%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,49 +1,35 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        output, i, carryBit = "", 1, 0
+        a, b = list(a), list(b)
+        a.reverse()
+        b.reverse()
+        
+        i, carry, res = 0, 0, []
 
-        while i <= len(a) and i <= len(b):
+        while i < len(a) or i < len(b):
+            SUM = 0
 
-            if int(a[-i]) + int(b[-i]) == 2:
-
-                if carryBit == 1: output = "1" + output
-                else:
-                    carryBit = 1
-                    output = "0" + output
-
-            elif int(a[-i]) + int(b[-i]) == 1:
-
-                if carryBit == 1: output = "0" + output
-                else: output = "1" + output
-            
-            else:
-                if carryBit == 1:
-                    carryBit -= 1
-                    output = "1" + output
-                else: output = "0" + output
-
+            if i < len(a): SUM += int(a[i])
+            if i < len(b): SUM += int(b[i])
+            if carry:
+                SUM += 1
+                carry -= 1
+            if SUM == 3:
+                res.append("1")
+                carry += 1
+            elif SUM == 2:
+                res.append("0")
+                carry += 1
+            elif SUM == 1:
+                res.append("1")
+            elif SUM == 0:
+                res.append("0")
+                
             i += 1
 
-        
-        if len(a) != len(b):
-            excess = a[:len(a) - len(b)] if len(a) > len(b) else b[:len(b) - len(a)]
-            if carryBit > 0:
-                for i in range(len(excess) -1, -1, -1):
-                    if int(excess[i]) + carryBit == 2:
-                        excess = excess[:i] + "0" + excess[i+1:]
+        while carry:
+            res.append("1")
+            carry -= 1
 
-                    else:
-                        carryBit -= 1
-                        excess = excess[:i] + "1" + excess[i+1:]
-                        break
- 
-                if carryBit == 1:
-                    carryBit -= 1  
-                    excess = "1" + excess
-                output = excess + output
-
-            else: output = excess + output
-
-        if carryBit > 0: return str(carryBit) + output
-
-        else: return output
+        res.reverse()
+        return ''.join(res)
