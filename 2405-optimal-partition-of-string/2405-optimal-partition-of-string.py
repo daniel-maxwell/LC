f9904dd6767a183c9d67a9857a6a87ff5a1c926c
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-05-12 01:05:52 +0100

    Time: 98 ms (61.24%), Space: 17.3 MB (72.33%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -1,12 +1,11 @@
 class Solution:
     def partitionString(self, s: str) -> int:
-        charSet = set()
-        result = 1
+        charSet, result = set(), 1
 
-        for i in range(len(s)):
-            if s[i] in charSet:
+        for char in s:
+            if char in charSet:
                 result += 1
                 charSet = set()
-            charSet.add(s[i])
+            charSet.add(char)
 
         return result
