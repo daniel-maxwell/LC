371cecd3459649ae0c3e2911bef9fd4a88d720c8
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-05-28 10:32:43 +0100

    Time: 59 ms (99.29%), Space: 19.2 MB (36.53%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -1,24 +1,20 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        counts = {}
-
-        for num in nums:
-            if num in counts:
-                counts[num] += 1
-            else:
-                counts[num] = 1
-
-        nums = list(set(nums))
+        counts = Counter(nums)
+        nums = set(nums)
+        nums = list(nums)
         nums.sort()
 
-        dp = [0] * len(nums)
-
-        dp[0] = nums[0] * counts[nums[0]]
+        dp = [nums[0] * counts[nums[0]]]
 
         for i in range(1, len(nums)):
-            if nums[i] - 1 == nums[i-1]:
-                dp[i] = max(dp[i-1], dp[i-2] + (nums[i] * counts[nums[i]]))
-            else:
-                dp[i] = dp[i-1] + (nums[i] * counts[nums[i]])
+            curr = nums[i] * counts[nums[i]]
 
+            if nums[i] - 1 not in counts:
+                dp.append(dp[-1] + curr)
+
+            else:
+                skipCurrent = (dp[-2] if i >= 2 else 0) + curr
+                dp.append(max(dp[-1], skipCurrent))
+        
         return dp[-1]
