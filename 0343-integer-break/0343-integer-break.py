c04bfaff5e376705204cc72e444b8a135dd22c90
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-05-30 08:28:54 +0100

    Time: 45 ms (15.74%), Space: 16.5 MB (44.23%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,36 +1,28 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-        maxProductMap = {}
-        res, coef = 0, 1
-
-        while n >= 10:
-            coef *= 3
+        a = 1
+        while n > 10:
             n -= 3
+            a *= 3
 
-        def backtrack(original, remaining, numsUsed):
+        b = 0
+        def dfs(nums, remaining):
 
-            if (remaining in maxProductMap or remaining == 0):
-                if remaining == 0 and len(numsUsed) == 1:
-                    return
-                nonlocal res
-                x = maxProductMap.get(remaining, 1)
+            if remaining == 0:
+                if len(nums) > 1:
+                    nonlocal b
+                    x = 1
+                    for num in nums:
+                        x *= num
+                    b = max(b, x)
+                return
 
-                for num in numsUsed:
-                    x *= num
-                res = max(res, x)
-                maxProductMap[original] = res
-                
+            
             for i in range(1, remaining + 1):
-                numsUsed.append(i)
-                if remaining - i >= 0:
-                    backtrack(original, remaining - i, numsUsed)
-                    numsUsed.pop()
-                else:
-                    break
-
-            return res
-
-        for i in range(1, n+1):
-            backtrack(i, i, [])
-
-        return coef * res
+                nums.append(i)
+                remaining -= i
+                dfs(nums, remaining)
+                remaining += nums.pop()
+        
+        dfs([], n)
+        return a * b
