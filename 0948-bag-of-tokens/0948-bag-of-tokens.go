3e6499d5462331cc7815f1f1d75a00245770d5b1
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-08-06 12:33:56 +0100

    Time: 4 ms (71.88%), Space: 2.9 MB (65.63%) - LeetHub

---
+++ 0948-bag-of-tokens/0948-bag-of-tokens.go
@@ -0,0 +1,23 @@
+func bagOfTokensScore(tokens []int, power int) int {
+    slices.Sort(tokens)
+    l, r := 0, len(tokens) - 1
+    score, maxScore := 0, 0
+
+    for l <= r {
+        if score == 0 && power < tokens[l] {
+            break
+        } else if power >= tokens[l] {
+            power -= tokens[l]
+            tokens[l]++
+            l++
+            score++
+            if score > maxScore { maxScore = score }
+        } else {
+            power += tokens[r]
+            tokens[r] = -tokens[r]
+            r--
+            score--
+        }
+    }
+    return maxScore
+}
