9e405a26b84e69a37d359af45c45d20866e8770c
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-08-23 13:04:23 +0100

    Time: 7 ms (99.28%), Space: 27.1 MB (75.45%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int i = 0, j = s.size() - 1;
-        int baseCase = s.size() % 2;
-        while ((j - i) > baseCase) {
-            char temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
-            ++i;
-            --j;
-        };
-    };
+        int l = 0;
+        int r = s.size() - 1;
+        while (l < r) {
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            ++l;
+            --r;
+        }
+        return;
+    }
 };
