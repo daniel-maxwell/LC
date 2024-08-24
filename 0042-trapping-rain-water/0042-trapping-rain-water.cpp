d49bf6b20d42529174cf8810259aaf77b2027872
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-08-24 16:19:17 +0100

    Time: 20 ms (7.62%), Space: 25.4 MB (13.25%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,43 +1,25 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int totalVolume = 0;
-        int l = 0;
-        int r;
-
-        while (l < height.size() - 1) {
-            int prevHeightL = (l > 0) ? height[l - 1] : 0;
-            int nextHeightL = (l < height.size() - 1) ? height[l+1] : 0;
-    
-            if (nextHeightL >= height[l]) {
-                ++l;
-                continue;
-            }
-
-            int highestBlockIdx = -1;
-            int highestBlockHeight = 0;
-            for (int i = l + 1; i < height.size(); ++i) {
-                if (height[i] > highestBlockHeight) {
-                    highestBlockHeight = height[i];
-                    highestBlockIdx = i;
-                }
-                if (height[i] >= height[l]) {
-                    break;
-                }
-            }
-            if (highestBlockIdx == -1) return totalVolume;
-            r = highestBlockIdx;
-
-            int ptr = l + 1;
-            int blocksInbetween = 0;
-            while (ptr < r) {
-                blocksInbetween += height[ptr];
-                ++ptr;
-            }
-            int volumeTrapped = min(height[l], height[r]) * (r - l - 1) - blocksInbetween;
-            totalVolume += volumeTrapped;
-            l = r;
+        vector<int> minHeight (height.size());
+        int maxHeightL = 0;
+        for (int i = 0; i < height.size(); ++i) {
+            maxHeightL = max(maxHeightL, height[i]);
+            minHeight[i] = maxHeightL;
         }
-        return totalVolume;
+        int maxHeightR = 0;
+        for (int i = height.size()-1; i >= 0; --i) {
+            maxHeightR = max(maxHeightR, height[i]);
+            minHeight[i] = min(minHeight[i], maxHeightR);
+        }
+
+        int totalWaterVolume = 0;
+
+        for (int i = 0; i < minHeight.size(); ++i) {
+            const int vol = minHeight[i] - height[i] > 0 ? minHeight[i] - height[i] : 0;
+            totalWaterVolume += vol;
+        }
+
+        return totalWaterVolume;
     }
 };
