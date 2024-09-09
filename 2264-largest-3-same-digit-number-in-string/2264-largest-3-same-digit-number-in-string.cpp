9dfec29c6e36e521041f29540cd43ce6535f1182
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-09-09 16:12:29 +0100

    Time: 4 ms (47.21%), Space: 9.5 MB (15.22%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
     string largestGoodInteger(string num) {
-        map<string, int> strMap {
-            {"999", 9},
-            {"888", 8},
-            {"777", 7},
-            {"666", 6},
-            {"555", 5},
-            {"444", 4},
-            {"333", 3},
-            {"222", 2},
-            {"111", 1},
-            {"000", 0},
-            {"", -1}
-        };
+        set<string> strSet {"999","888", "777", "666", "555", "444", "333", "222", "111", "000"};
         string result = "";
         for (int i = 0; i < num.size(); ++i) {
             const string curr = num.substr(i, 3);
-            if (strMap.contains(curr) && strMap[curr] > strMap[result]) {
+            if (strSet.contains(curr) && curr > result) {
                 result = curr;
             }
         }
