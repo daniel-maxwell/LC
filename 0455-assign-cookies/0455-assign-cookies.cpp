af8471125a5d9e5b40c29d6768219f2cf849a6c5
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-09-18 20:33:31 +0100

    Time: 32 ms (38.19%), Space: 44.9 MB (10.17%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,3 +1,10 @@
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(nullptr);
+    std::cin.tie(nullptr);
+    return true;
+}();
+
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
