145d4964d32b79ec804912d33cdd6ad267247471
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-09-19 23:45:19 +0100

    Time: 4 ms (47.34%), Space: 8.6 MB (5.80%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,12 +1,23 @@
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(nullptr);
+    std::cin.tie(nullptr);
+    return true;
+}();
 class Solution {
 public:
+    inline void rtrim(std::string &s) {
+        s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+            return !std::isspace(ch);
+        }).base(), s.end());
+    }
+    inline void trim(std::string &s) { rtrim(s); }
     int lengthOfLastWord(string s) {
-        s.erase(s.find_last_not_of(" \n\r\t") + 1);
-        if (s.size() == 1) return 1;
-        int finalDelimIndex = -1;
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == ' ') finalDelimIndex = i;
-        }
-        return s.substr(finalDelimIndex+1, string::npos).size();
+        rtrim(s);
+        string stringStore;
+        stringstream ss(s);
+        string prev;
+        while (getline(ss, s, ' ')) prev = s;
+        return prev.size();
     }
 };
