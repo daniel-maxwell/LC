c6c62824e58983f6d0628031da7e2aba7b3399e0
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-09-21 17:13:54 +0100

    Time: 7 ms (25.92%), Space: 13 MB (13.09%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -1,12 +1,18 @@
+#pragma once
+static const bool _ = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(nullptr);
+    std::cin.tie(nullptr);
+    return true;
+}();
 class Solution {
 public:
-    int arraySign(vector<int>& nums) {
-        int negatives = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] == 0) return 0;
-            if (nums[i] < 0) ++negatives;
+    int arraySign(const vector<int>& nums) {
+        bool neg = false;
+        for (const int n : nums) {
+            if (n == 0) return 0;
+            if (n < 0) neg = !neg;
         }
-        if (negatives % 2 == 1) return -1;
-        else return 1;
-    };
+        return neg ? -1 : 1;
+    }
 };
