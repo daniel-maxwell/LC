9c2242868c175822bade56884f1dbb47ce382f82
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-09-27 23:52:28 +0100

    Time: 6 ms (54.69%), Space: 15.5 MB (35.96%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -1,7 +1,13 @@
+static const bool a() {
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(nullptr);
+    std::cin.tie(nullptr);
+    return true;
+}
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        nums.insert(nums.end(), nums.begin(), nums.end());
+        nums.insert(nums.end(), nums.begin(), nums.end() );
         return nums;
     }
 };
