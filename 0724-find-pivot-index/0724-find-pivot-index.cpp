ecbc08982a43d49487dc12ee1f3abd24bee83635
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-10-01 23:58:38 +0100

    Time: 8 ms (96.72%), Space: 34.6 MB (70.37%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,11 +1,18 @@
+static const bool a() {
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(nullptr);
+    std::cin.tie(nullptr);
+    return true;
+}
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int leftSum = 0, rightSum = accumulate(nums.begin(), nums.end(), 0);
+        int sumRight = 0, sumLeft = 0;
+        for (const int num : nums) sumRight += num;
         for (int i = 0; i < nums.size(); ++i) {
-            rightSum -= nums[i];
-            if (leftSum == rightSum) return i;
-            leftSum += nums[i];
+            sumRight -= nums[i];
+            if (sumLeft == sumRight) return i;
+            sumLeft += nums[i];
         }
         return -1;
     }
