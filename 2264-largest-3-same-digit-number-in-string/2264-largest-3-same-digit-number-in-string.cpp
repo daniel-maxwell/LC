3c00a66990012dbe7b161caec5dcd43a729449e3
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-10-03 23:49:57 +0100

    Time: 2 ms (60.16%), Space: 9 MB (28.44%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -1,14 +1,21 @@
+static const bool a() {
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(nullptr);
+    std::cin.tie(nullptr);
+    return true;
+}
 class Solution {
 public:
     string largestGoodInteger(string num) {
-        set<string> strSet {"999","888", "777", "666", "555", "444", "333", "222", "111", "000"};
-        string result = "";
-        for (int i = 0; i < num.size(); ++i) {
-            const string curr = num.substr(i, 3);
-            if (strSet.contains(curr) && curr > result) {
-                result = curr;
+        int result = -1;
+        for (int i = 2; i < num.size(); ++i) {
+            if (num[i] == num[i-1] && num[i] == num[i-2]) {
+                string str(1, num[i]);
+                str += num[i-1]; str += num[i-2];
+                result = max(result, std::stoi(str));
             }
         }
-        return result;
+        if (result == -1) return "";
+        return result == 0 ? "000" : std::to_string(result);
     }
 };
