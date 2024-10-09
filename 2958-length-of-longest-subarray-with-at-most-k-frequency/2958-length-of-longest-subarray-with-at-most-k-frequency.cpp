b29963b09b409e8a4b1e4f0e0a01022d07284b67
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-10-09 11:54:11 +0100

    Time: 155 ms (98.79%), Space: 140.1 MB (99.94%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,16 +1,28 @@
-static const bool a() {
-    std::ios_base::sync_with_stdio(false);
-    std::cout.tie(nullptr);
-    std::cin.tie(nullptr);
-    return true;
+auto init = [](){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    return 'c';
+}();
+const size_t BUFFER_SIZE = 0x6fafffff;
+alignas(std::max_align_t) char buffer[BUFFER_SIZE];
+size_t buffer_pos = 0;
+void* operator new(size_t size) {
+    constexpr std::size_t alignment = alignof(std::max_align_t); // Default maximum alignment
+    size_t padding = (alignment - (buffer_pos % alignment)) % alignment;
+    size_t total_size = size + padding;
+    char* aligned_ptr = &buffer[buffer_pos + padding];
+    buffer_pos += total_size;
+    return aligned_ptr;
 }
+void operator delete(void* ptr) {}
+void operator delete[](void* ptr) {}
+
 class Solution {
 public:
     const int maxSubarrayLength(const vector<int>& nums, const int k) {
         int l = 0, r = 0;
         int result = 0;
         int currMaxFreq = 1;
-        map<int, int> freqs;
+        unordered_map<int, int> freqs;
         while (r < nums.size()) {
             ++freqs[nums[r]];
             currMaxFreq = max(currMaxFreq, freqs[nums[r]]);
