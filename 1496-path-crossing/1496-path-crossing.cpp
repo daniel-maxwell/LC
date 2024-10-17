dcd6e0a72e651ffcdfd4f9c20a7eb93d98404d6a
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-10-17 21:56:42 +0100

    Time: 0 ms (100.00%), Space: 8.8 MB (35.64%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.cpp
+++ 1496-path-crossing/1496-path-crossing.cpp
@@ -1,6 +1,10 @@
+auto init = [](){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    return 'c';
+}();
 class Solution {
 public:
-    bool isPathCrossing(string path) {
+    const bool isPathCrossing(const string path) {
         map<char, pair<int, int>> directions {
             {'N', {-1, 0}},
             {'E', {0, 1}},
@@ -9,7 +13,7 @@ public:
         };
         set<pair<int, int>> visited;
         pair<int, int> curr {0, 0};
-        for (const char c : path) {
+        for (const char& c : path) {
             visited.insert(curr);
             curr.first += directions[c].first;
             curr.second += directions[c].second;
