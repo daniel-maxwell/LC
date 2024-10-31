8d50dac56c4208bb5f7533bfb7d5bfcfc0d48406
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-10-31 23:35:26 +0000

    Time: 3 ms (66.36%), Space: 10 MB (16.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,14 @@
+auto init = [](){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    return 'c';
+}();
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<char> x (s.begin(), s.end());
-        vector<char> y (t.begin(), t.end());
-        std::sort(x.begin(), x.end());
-        std::sort(y.begin(), y.end());
-        string a (x.begin(), x.end());
-        string b (y.begin(), y.end());
-        return a == b;
+        map<char, int> sMap;
+        map<char, int> tMap;
+        for (const char& sChar : s) ++sMap[sChar];
+        for (const char& tChar : t) ++tMap[tChar];
+        return sMap == tMap;
     }
 };
