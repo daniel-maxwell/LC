c861020dac13e83c653df0ee199fbb8e51432405
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-11-06 23:35:14 +0000

    Time: 0 ms (100.00%), Space: 27.1 MB (86.46%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,15 +1,1 @@
-class Solution {
-public:
-    void reverseString(vector<char>& s) {
-        int l = 0;
-        int r = s.size() - 1;
-        while (l < r) {
-            char temp = s[l];
-            s[l] = s[r];
-            s[r] = temp;
-            ++l;
-            --r;
-        }
-        return;
-    }
-};
+class Solution {public: void reverseString(vector<char>& s) { reverse(s.begin(), s.end());}};
