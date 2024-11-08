ef04ef2ea566a90e68034eb0218420d12b1378c1
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-11-08 18:48:24 +0000

    Time: 8 ms (9.68%), Space: 9.5 MB (66.71%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,15 +1,18 @@
+auto init = [](){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    return 'c';
+}();
 class Solution {
 public:
-    bool isIsomorphic(string s, string t) {
-        unordered_map<char, char> mapST, mapTS;
+    const bool isIsomorphic(const string s, const string t) {
+        map<char, char> sToT;
+        map<char, char> tToS;
         for (int i = 0; i < s.size(); ++i) {
-            if ((mapST.find(s[i]) != mapST.end() && mapST.at(s[i]) != t[i]) ||
-                (mapTS.find(t[i]) != mapTS.end() && mapTS.at(t[i]) != s[i])) {
-                return false;
-            }
-            mapST.insert_or_assign(s[i], t[i]);
-            mapTS.insert_or_assign(t[i], s[i]);
-        };
+            if (sToT.contains(s[i]) && sToT[s[i]] != t[i]) return false;
+            if (tToS.contains(t[i]) && tToS[t[i]] != s[i]) return false;
+            sToT[s[i]] = t[i];
+            tToS[t[i]] = s[i];
+        }
         return true;
-    };
+    }
 };
