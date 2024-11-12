7e7db5dbc671103eb649b7cda7add31b109ed00d
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-11-12 22:45:36 +0000

    Time: 3 ms (17.88%), Space: 9.3 MB (18.23%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,3 +1,7 @@
+const auto init = [](){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    return 'c';
+}();
 class Solution {
 public:
     const char findTheDifference(const string s, const string t) {
