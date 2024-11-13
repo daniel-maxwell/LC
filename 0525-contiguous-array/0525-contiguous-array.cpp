a66d9168c35a09c68da6195a4efffbf9259e6bb5
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-11-13 12:09:15 +0000

    Time: 31 ms (49.53%), Space: 88.3 MB (68.70%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        if (nums.size() == 1) return 0;
+        unordered_map<int, int> diffs;
+        int oneCount = 0, zeroCount = 0, result = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] == 1) ++oneCount;
+            else ++zeroCount;
+
+            if (!diffs.contains(oneCount - zeroCount)) {
+                diffs[oneCount - zeroCount] = i;
+            }
+            
+            if (oneCount == zeroCount) {
+                result = oneCount + zeroCount;
+            } else {
+                if (diffs.contains(oneCount - zeroCount)) {
+                    result = max(result, i - diffs[oneCount - zeroCount]);
+                }
+            }
+        }
+        return result;
+    }
+};
