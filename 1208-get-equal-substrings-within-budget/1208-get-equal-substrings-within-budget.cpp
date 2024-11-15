296cd1a94bda49babfe7eb4692f8334dcad72455
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-11-15 13:58:53 +0000

    Time: 3 ms (31.39%), Space: 8.6 MB (100.00%) - LeetHub

---
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    const int equalSubstring(const string &s, const string &t, const int maxCost) {
+        int l = 0, r = 0;
+        int budget = maxCost, result = 0;
+        while (r < s.size()) {
+            int cost = getCost(s[r], t[r]);
+            if (budget >= cost) {
+                budget -= cost;
+                result = max(result, r - l + 1);
+                ++r;
+            } else {
+                if (l == r) {
+                    ++l;
+                    ++r;
+                    continue;
+                }
+                budget += getCost(s[l], t[l]);
+                ++l;
+            }
+        }
+        return result;
+    }
+
+    const int getCost(const char &a, const char &b) {
+        int costA = a - 'a', costB = b - 'a';
+        return abs(costA - costB);
+    }
+};
