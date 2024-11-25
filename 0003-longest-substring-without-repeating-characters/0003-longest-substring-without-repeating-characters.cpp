45742bbe5cabce2ef3b394a4d3f6c109aaa01efe
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-11-25 23:27:41 +0000

    Time: 23 ms (24.25%), Space: 16.5 MB (11.01%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,22 @@
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false), std::cout.tie(nullptr), std::cin.tie(nullptr);
+    return true;
+}();
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        set<char> charSet;
+        int l = 0, r = 0;
+        int result = 0;
+        while (r < s.size()) {
+            while (charSet.contains(s[r])) {
+                charSet.erase(s[l]);
+                ++l;
+            }
+            charSet.insert(s[r]);
+            ++r;
+            result = max(result, r - l);
+        }
+        return result;
+    }
+};
