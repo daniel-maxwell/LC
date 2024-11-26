36b432b8b0b1a07d8ff951e6cd74a8cf346357ed
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-11-26 22:35:18 +0000

    Time: 503 ms (5.19%), Space: 106.3 MB (13.78%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,3 +1,7 @@
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false), std::cout.tie(nullptr), std::cin.tie(nullptr);
+    return true;
+}();
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
