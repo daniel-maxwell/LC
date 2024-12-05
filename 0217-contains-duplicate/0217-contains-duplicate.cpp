7922f73b52aad2dd7ca16cb923fb386745bd1697
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-12-05 22:38:51 +0000

    Time: 31 ms (65.97%), Space: 73.5 MB (30.80%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,3 +1,7 @@
+static const bool Booster = [](){
+    std::ios_base::sync_with_stdio(false), std::cout.tie(nullptr), std::cin.tie(nullptr);
+    return true;
+}();
 class Solution {
 public:
     const bool containsDuplicate(const vector<int>& nums) {
