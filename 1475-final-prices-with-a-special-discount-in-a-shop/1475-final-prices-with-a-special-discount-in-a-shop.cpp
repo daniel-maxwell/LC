82d769eab0065cbb8cd1a49302fe4765396c52ad
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2024-12-18 23:36:21 +0000

    Time: 2 ms (19.13%), Space: 14.1 MB (25.89%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        for (int i = 0; i < prices.size() - 1; ++i) {
+            int j = i + 1;
+            while (j < prices.size() && prices[j] > prices[i]) ++j;
+            if (j < prices.size()) {
+                prices[i] -= prices[j];
+            }
+        }
+        return prices;
+    }
+};
