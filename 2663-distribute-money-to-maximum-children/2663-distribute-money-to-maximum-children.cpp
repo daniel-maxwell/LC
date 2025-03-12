9acd44be06c5a6c5edb7337210acff13aba4c3e7
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2025-03-12 19:29:26 +0100

    Time: 3 ms (12.47%), Space: 11 MB (3.7%) - LeetHub

---
+++ 2663-distribute-money-to-maximum-children/2663-distribute-money-to-maximum-children.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int distMoney(int money, int children) {
+
+        if (money < children) {
+            return -1;
+        }
+
+        money -= children;
+
+        vector<int> childBalances (children, 1);
+
+        int result = 0;
+        int i = 0;
+
+        while (i < childBalances.size()) {
+            if (money < 7) {
+                if (childBalances[i] + money == 4) { 
+                    if (i == childBalances.size() - 1) {  // We're at the end of the array
+                        --result;
+                    }
+                } else {
+                    childBalances[i] += money;
+                    money == 0;
+                }
+                return result;
+            } else {
+                childBalances[i] += 7;
+                ++result;
+                money -= 7;
+            }
+            ++i;
+        }
+
+        if (money > 0) --result;
+        return result;
+    }
+};
