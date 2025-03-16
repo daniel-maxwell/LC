22606fcb6de70604277414e823c3c849cc7be4ee
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2025-03-16 11:49:22 +0100

    Time: 1770 ms (5%), Space: 18.7 MB (97.44%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,47 +1,43 @@
-static const auto _ = std::cin.tie(nullptr)->sync_with_stdio(false);
-#define LC_HACK
-#ifdef LC_HACK
-const auto __ = []() {
-    struct ___ {
-        static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; }
-    };
-    std::atexit(&___::_);
-    return 0;
-}();
-#endif
 class Solution {
 public:
     const int ladderLength(const string beginWord, const string endWord, const vector<string>& wordList) {
-        unordered_set<string> visited {beginWord};
-        queue<string> q; 
+        
+        unordered_set<string> visited;
+        queue<string> q;
         q.push(beginWord);
-        int result = 1;
-        while (!q.empty()) {
-            const int length = q.size();
-            for (int _ = 0; _ < length; ++_) {
-                const string cur = q.front();
-                if (cur == endWord) return result;
+        int depth = 1;
+
+        while (!q.empty() && depth <= wordList.size()) {
+            int length = q.size();
+            for (int i = 0; i < length; ++i) {
+                const string curWord = q.front();
                 q.pop();
                 for (const string &word : wordList) {
                     if (visited.contains(word)) continue;
-                    if (canTraverse(cur, word)) {
-                        visited.insert(word);
+                    if (differsByOneLetter(curWord, word)) {
+                        if (word == endWord) {
+                            return depth + 1;
+                        }
                         q.push(word);
+                        visited.insert(word);
                     }
                 }
             }
-            ++result;
+            ++depth;
         }
+
         return 0;
+
     }
 
-private:
-    const bool canTraverse(const string &s, const string &t) {
-        bool differs = false;
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] != t[i]) {
-                if (differs) return false;
-                differs = true;
+    const bool differsByOneLetter(const string &word1, const string &word2) {
+        bool foundDifference = false;
+        for (int i = 0; i < word1.size(); ++i) {
+            if (word1[i] != word2[i]) {
+                if (foundDifference) {
+                    return false;
+                }
+                foundDifference = true;
             }
         }
         return true;
