b5480f5d81e86da2c6e836448b433a623d71c154
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2025-03-20 16:31:40 +0000

    Time: 13 ms (5.38%), Space: 102.8 MB (5.38%) - LeetHub

--- 2422-merge-operations-to-turn-array-into-a-palindrome/2422-merge-operations-to-turn-array-into-a-palindrome.cpp
+++ 2422-merge-operations-to-turn-array-into-a-palindrome/2422-merge-operations-to-turn-array-into-a-palindrome.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
+
+        vector<long> longNums;
+
+        for (const int & n : nums) {
+            longNums.push_back(static_cast<long>(n));
+        }
         int numOperations = 0;
         int i = 0, j = nums.size() - 1;
-        long long left = static_cast<long long>(nums[i]);
-        long long right = static_cast<long long>(nums[j]);
         while (i < j) {
-            if (left == right) {
+            if (longNums[i] == longNums[j]) {
                 ++i;
                 --j;
-                left = static_cast<long long>(nums[i]);
-                right = static_cast<long long>(nums[j]);
                 continue;
             }
-            if (left < right) {
-                left = static_cast<long long>(nums[i + 1]) + static_cast<long long>(nums[i]);
+            if (longNums[i] < longNums[j]) {
+                longNums[i+1] += longNums[i];
                 ++i;
             } else {
-                right = static_cast<long long>(nums[j - 1]) + static_cast<long long>(nums[j]);
+                longNums[j-1] += longNums[j];
                 --j;
             }
             ++numOperations;
