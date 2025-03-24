621e2b821f07d03971f7a3fc4d24c5966d39ccf3
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2025-03-24 13:49:57 +0000

    Time: 91 ms (35.12%), Space: 84.1 MB (21.09%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-
-        vector<pair<double, int>> pointsHeap;
-
-        for (int i = 0; i < points.size(); ++i) {
-            const pair<double, int> dist {
-                calcDistance(points[i][0], points[i][1], 0, 0), i
-            };
-            pointsHeap.push_back(dist);
-            push_heap(pointsHeap.begin(), pointsHeap.end());
+        vector<pair<double, vector<int>>> pointsHeap;
+        for (const vector<int> &point : points) {
+            pointsHeap.push_back(pair<double, vector<int>>{-getEuclidianDist(point[0], point[1]), point});
         }
+
+        make_heap(pointsHeap.begin(), pointsHeap.end());
 
         vector<vector<int>> result;
 
-        for (int i = 0; i < k; ++i) {
+        while (k > 0) {
             pop_heap(pointsHeap.begin(), pointsHeap.end());
-            const vector<int> point = points[pointsHeap.back().second];
-            result.push_back(point);
+            result.push_back(pointsHeap.back().second);
             pointsHeap.pop_back();
+            --k;
         }
 
         return result;
     }
 
-    const double calcDistance(int x1, int y1, int x2, int y2) {
-        return -sqrt(pow(double(x1) - double(x2), 2) + pow(double(y1) - double(y2), 2));
+    double getEuclidianDist(int x, int y) {
+        return sqrt(double(pow(x, 2)) + double(pow(y, 2)));
     }
 };
