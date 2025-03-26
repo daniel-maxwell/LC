8359becf2de57e1bb4bb78bcc2d2ce2fd3431bed
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2025-03-26 17:39:38 +0000

    Time: 0 ms (100%), Space: 10.2 MB (78.2%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if (nums.size() == 1) {
-            return nums.front();
-        }
-        int result = max(nums[0], nums[1]);
-        int maxPrev = nums[0];      
+        if (nums.size() >= 2) nums[1] = max(nums[0], nums[1]);
+        else return nums.back();
         for (int i = 2; i < nums.size(); ++i) {
-            int temp = max(maxPrev + nums[i], result);
-            maxPrev = result;
-            result = temp;
+            nums[i] = max(nums[i-2] + nums[i], nums[i-1]);
         }
-        return result;
+        return nums.back();
     }
 };
