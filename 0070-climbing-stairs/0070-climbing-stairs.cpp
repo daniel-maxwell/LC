e73467f9d295e451907d44b3f11ce0c10354a635
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2025-03-26 14:28:31 +0000

    Time: 0 ms (100%), Space: 7.9 MB (71.46%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,22 +1,13 @@
-static const bool Booster = [](){
-    std::ios_base::sync_with_stdio(false);
-    std::cout.tie(nullptr);
-    std::cin.tie(nullptr);
-    return true;
-}();
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n <= 2) return n;
         int a = 1;
         int b = 2;
-        n -= 2;
-        while (n > 0) {
-            int temp = a + b;
+        for (int i = 2; i < n; ++i) {
+            const int temp = a + b;
             a = b;
             b = temp;
-            --n;
         }
-        return b;
+        return n == 1 ? a : b;
     }
 };
