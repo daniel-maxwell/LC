fa8bdc1ff4a385ab88ab77346bc85fc17278e706
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2025-04-01 03:59:35 +0100

    Time: 15 ms (72.78%), Space: 26 MB (56.28%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> m;
-        for (int i = 0; i < strs.size(); i++) {
-            string key = getKey(strs[i]);
-            m[key].push_back(strs[i]);
-        }
-        
-        vector<vector<string>> result;
-        for (auto it = m.begin(); it != m.end(); it++) {
-            result.push_back(it->second);
-        }
-        return result;
-    }
-private:
-    string getKey(string str) {
-        vector<int> count(26, 0);
-        for (char c : str) {
-            count[c - 'a']++;
+
+        unordered_map<string, vector<string>> sortedMap;
+
+        for (const string &s : strs) {
+            string sortedS = s;
+            sort(sortedS.begin(), sortedS.end());
+            sortedMap[sortedS].push_back(s);
         }
 
-        string key = "";
-        for (int i = 0; i < 26; i++) {
-            key.append("#");
-            key.append(to_string(count[i]));
+        vector<vector<string>> result;
+
+        for (const pair<string, vector<string>> &entry : sortedMap) {
+            result.push_back(entry.second);
         }
-        return key;
-    };
+
+        return result;
+    }
 };
