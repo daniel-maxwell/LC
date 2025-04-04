32848ab3a51a1ff82f7dccc32bb1f57d1f393412
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2025-04-04 12:11:43 +0100

    Time: 0 ms (100%), Space: 10.8 MB (16.32%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,12 +1,12 @@
-auto init = [](){
+static const bool init = [](){
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    return 'c';
+    return true;
 }();
 class Solution {
 public:
     const bool isPalindrome(const int x) {
         const string strX = to_string(x);
-        size_t l = 0, r = strX.size() - 1;
+        int l = 0, r = strX.size() - 1;
         while (l < r) {
             if (strX[l] != strX[r]) return false;
             ++l;
