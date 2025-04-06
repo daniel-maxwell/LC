ff3fb31dafc20de56fe7feb1c4c872763ba53c5e
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2025-04-06 21:15:09 +0100

    Time: 3 ms (10.78%), Space: 17.3 MB (16.25%) - LeetHub

---
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int result = 0;
+
+        for (const string &s : details) {
+            string age = s.substr(11, 2);
+            if (stoi(age) > 60) ++result;
+        }
+        
+        return result;
+    }
+};
