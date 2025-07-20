eb6c9c9a9c333d3a7da43907b185e989bb3173f6
DanielMW <66431847+daniel-maxwell@users.noreply.github.com>
2025-07-20 12:39:35 +0100

    Time: 2 ms (42.82%), Space: 8.7 MB (15.9%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -6,7 +6,15 @@ public:
         size_t i = 0;
         while (i < word1.size() || i < word2.size()) {
             if (i < word1.size()) s += word1[i];
+            else {
+                s += word2.substr(i, word2.size() - i);
+                break;
+            }
             if (i < word2.size()) s += word2[i];
+            else {
+                s += word1.substr(i + 1, word1.size() - i);
+                break;
+            }
             ++i;
         }
         return s;
